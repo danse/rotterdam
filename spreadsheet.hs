6a77d14df080eefb4e25ed03673d71fca2a35ed8
danse <f.occhipinti@gmail.com>
2018-07-11 10:53:17 +0200

    Mi 11. Jul 10:53:17 CEST 2018

---
+++ spreadsheet.hs
@@ -0,0 +1,4 @@
+spesi ricevuti saldo = sum ricevuti - saldo
+disponibili previste ricevuti totale saldo = totale -
+                                             sum previste -
+                                             spesi ricevuti saldo
