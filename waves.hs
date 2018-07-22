ce2bd01837899c5ea2380cc7bdbd11a89108dd6d
danse <f.occhipinti@gmail.com>
2018-07-22 16:21:52 +0200

    So 22. Jul 16:21:52 CEST 2018

--- waves.hs
+++ waves.hs
@@ -22,6 +22,12 @@
 
 
 
+-- s = Stages [Stage [Batch "batch or bit"]]
+
+data Stages = Stages [Stage] deriving (Show, Read)
+data Stage = Stage [Batch] deriving (Show, Read)
+data Batch = Batch String deriving (Show, Read)
+
 spesi ricevuti saldo = sum ricevuti - saldo
 
 disponibili previste totale spesi = totale - sum previste - spesi
