f55a4715d5293d5820d82f7b37ed69525cd1b1d0
danse <f.occhipinti@gmail.com>
2019-05-13 12:41:40 +0200

    Mo 13. Mai 12:41:40 CEST 2019

--- parsing/simple-parsing.hs
+++ parsing/simple-parsing.hs
@@ -3,9 +3,6 @@ import Text.Parsec
 import Text.Parsec.Token
 import Data.Functor
 
-data Norm = Legge | DecretoLegislativo
-data Reference = Reference { norm :: Norm, serial :: Int, year :: Int }
-
 parser :: Stream s m Char => ParsecT s u m String
 parser = string "legge"
 
